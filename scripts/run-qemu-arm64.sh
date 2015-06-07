f9364f47d809526336e7bb3cbf1e4854193b7157
Dieter Reuter <dieter.reuter@me.com>
2015-06-07 13:13:24 +0200

    Reduce memory for QEMU machine to 512mb

--- scripts/run-qemu-arm64.sh
+++ scripts/run-qemu-arm64.sh
@@ -5,7 +5,7 @@ export MAC_ADDRESS=$(printf 'DE:AD:BE:EF:%02X:%02X\n' $((RANDOM%256)) $((RANDOM%
 echo "MAC_ADDRESS=$MAC_ADDRESS"
 
 # run the QEMU box
-sudo qemu-system-aarch64 -m 768 -cpu cortex-a57 -M virt -nographic \
+sudo qemu-system-aarch64 -m 512 -cpu cortex-a57 -M virt -nographic \
   -pflash $HOME/qemu/bootdisk/flash0.img \
   -pflash $HOME/qemu/bootdisk/flash1.img \
   -drive if=none,file=$HOME/qemu/images/arm64/vivid-server-cloudimg-arm64-uefi1-modified.img,id=hd0 \
